77ef0f93c111c07d7ac8eeaf920eff5b885359eb
youngsikhcoder <vihaan.jabbal@gmail.com>
2026-06-26 17:58:05 -0500

    I made a car speed

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
-input.onButtonPressed(Button.A, function () {
-	
-})
+BitBuggy.init_wheel(AnalogPin.P1, AnalogPin.P2)
 basic.forever(function () {
-    basic.showIcon(IconNames.Heart)
+    basic.pause(1000)
+    BitBuggy.forward()
+    basic.pause(100)
+    RingbitCar.turnright()
 })
