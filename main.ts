215396a5b2db23f659a78335aeda0577ec65a52e
youngsikhcoder <vihaan.jabbal@gmail.com>
2026-07-11 17:11:42 -0500

    I made a Robot cleaner so it could clean everything

--- main.ts
+++ main.ts
@@ -1,7 +1,18 @@
-BitBuggy.init_wheel(AnalogPin.P1, AnalogPin.P2)
+let Start: kitronik_halo_hd.ZIPHaloHd = null
+let strip = 0
+let Vacuum = 0
+radio.onReceivedNumber(function (receivedNumber) {
+    music.play(music.stringPlayable("C5 G C G E G F C ", 120), music.PlaybackMode.UntilDone)
+    pins.analogSetPeriod(AnalogPin.P3, 200)
+})
 basic.forever(function () {
-    basic.pause(1000)
-    BitBuggy.forward()
-    basic.pause(100)
-    RingbitCar.turnright()
+    datalogger.includeTimestamp(FlashLogTimeStampFormat.Milliseconds)
+    RingbitCar.init_wheel(AnalogPin.P16, AnalogPin.P0)
+    Start = kitronik_halo_hd.createZIPHaloDisplay(43)
+})
+basic.forever(function () {
+    basic.showString("Hello!")
+    strip = 34
+    servos.P0.setStopOnNeutral(true)
+    Vacuum = 54
 })
