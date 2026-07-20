9d6334b0aafbb897b5d589384a48a419d0c33b8b
youngsikhcoder <vihaan.jabbal@gmail.com>
2026-07-20 16:33:53 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,15 @@
-let Start: kitronik_halo_hd.ZIPHaloHd = null
-let strip = 0
-let Vacuum = 0
-radio.onReceivedNumber(function (receivedNumber) {
-    music.play(music.stringPlayable("C5 G C G E G F C ", 120), music.PlaybackMode.UntilDone)
-    pins.analogSetPeriod(AnalogPin.P3, 200)
+basic.forever(function () {
+    RingbitCar.init_wheel(AnalogPin.P1, AnalogPin.P2)
 })
 basic.forever(function () {
-    datalogger.includeTimestamp(FlashLogTimeStampFormat.Milliseconds)
-    RingbitCar.init_wheel(AnalogPin.P16, AnalogPin.P0)
-    Start = kitronik_halo_hd.createZIPHaloDisplay(43)
+    RingbitCar.turnright()
 })
 basic.forever(function () {
-    basic.showString("Hello!")
-    strip = 34
-    servos.P0.setStopOnNeutral(true)
-    Vacuum = 54
+    RingbitCar.forward()
+})
+basic.forever(function () {
+    RingbitCar.turnleft()
+})
+basic.forever(function () {
+    RingbitCar.back()
 })
